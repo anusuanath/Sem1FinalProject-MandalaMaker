fb2ebd0e9d4d1e5634aca36cbe7a5d0e2080539c
rachel-ng <rng@stuy.edu>
2018-01-08 15:12:40 -0500

    changed color of squares to blue

--- mainDraw.java
+++ mainDraw.java
@@ -10,7 +10,8 @@ public class mainDraw extends JComponent {
 
     public int x = 100;
     public int y = 100;
-    
+
+    /*
     public void paint(Graphics g) {
 	super.paint(g);
 	g.fillRect(x, y, 10, 10);
@@ -22,12 +23,12 @@ public class mainDraw extends JComponent {
 	g.fillRect(x-20, y-20, 10, 10);
 	g.fillRect(x, y+20, 10, 10);
 	g.fillRect(x, y-20, 10, 10);
-        g.setColor(Color.RED);
     }
-
-    /*
+    */
+    
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+	g.setColor(Color.blue);
         g.fillRect(x, y, 10, 10);
 	g.fillRect(x+20, y+20, 10, 10);
 	g.fillRect(x+20, y, 10, 10);
@@ -37,9 +38,7 @@ public class mainDraw extends JComponent {
 	g.fillRect(x-20, y-20, 10, 10);
 	g.fillRect(x, y+20, 10, 10);
 	g.fillRect(x, y-20, 10, 10);
-        g.setColor(Color.BLACK);
     }
-    */
 
     public void moveRight() {
         if (x + 50 < 600) {
