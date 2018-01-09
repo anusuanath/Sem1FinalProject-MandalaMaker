bf888a6db5e043eec8e0e80618328d0b93446a8f
rachel-ng <rng@stuy.edu>
2018-01-09 15:30:51 -0500

    found an easy painting thing

--- SimplePaint.java
+++ SimplePaint.java
@@ -3,32 +3,11 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-/**
- * A simple applet where the user can sketch curves in a variety of
- * colors.  A color palette is shown along the right edge of the applet.
- * The user can select a drawing color by clicking on a color in the
- * palette.  Under the colors is a "Clear button" that the user
- * can press to clear the sketch.  The user draws by clicking and
- * dragging in a large white area that occupies most of the applet.
- * The user's drawing is not persistent.  It is lost whenever
- * the applet is repainted for any reason.
- * <p>The drawing that is done in this example violates the rule
- * that all drawing should be done in the paintComponent() method.
- * Although it works, it is NOT good style.
- * <p>This class also contains a main program, and it can be run as
- * a stand-alone application that has exactly the same functionality
- * as the applet.
- */
+/* https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0ahUKEwikx-D73MvYAhVDPN8KHbtPApQQFggmMAA&url=http%3A%2F%2Fmath.hws.edu%2Fjavanotes6%2Fsource%2FSimplePaint.java&usg=AOvVaw3ymy4S_BNoh9vU5UR0Mo_9  */
+
 public class SimplePaint extends JApplet {
-   
-   /**
-    * The main routine opens a window that displays a drawing area
-    * and color palette.  This main routine allows this class to
-    * be run as a stand-alone application as well as as an applet.
-    * The main routine has nothing to do with the function of this
-    * class as an applet.
-    */
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
       JFrame window = new JFrame("Simple Paint");
       SimplePaintPanel content = new SimplePaintPanel();
       window.setContentPane(content);
@@ -36,16 +15,10 @@ public class SimplePaint extends JApplet {
       window.setLocation(100,100);
       window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
       window.setVisible(true);
-
    }
    
-   /**
-    * The init method of the applet simply creates a SimplePaintPanel and
-    * uses it as the content pane of the applet.  All the work is done
-    * by the SimplePaintPanel.
-    */
    public void init() {
-      setContentPane( new SimplePaintPanel() );
+      setContentPane(new SimplePaintPanel());
    }
    
    
