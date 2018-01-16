e9b9bc4d752f1f263197e9da2349933a6088d1d2
rng <rng@stuy.edu>
2018-01-16 03:07:14 -0500

    me, at an aa meeting: hi, my name is rachel and black coffee no longer has an effect on me

--- CGMoveALine.java
+++ CGMoveALine.java
@@ -11,15 +11,38 @@ public class CGMoveALine extends JFrame {
     private int x1 = 10;
     private int y1 = 50;
     private int x2 = 10;
-    private int y2 = 60;
+    private int y2 = 100;
  
     private DrawCanvas canvas;
 
     public CGMoveALine() {
 
 	JPanel btnPanel = new JPanel(new FlowLayout());
-	JButton btnLeft = new JButton("Move Left ");
-	btnPanel.add(btnLeft);
+	JLabel lAxes = new JLabel("# Axes");
+	JTextField txtAxes = new JTextField(2);
+	JLabel lColor = new JLabel("Color: ");
+
+	DefaultListModel colors = new DefaultListModel();
+	colors.addElement("black");
+	colors.addElement("white"); 
+	colors.addElement("red");
+	colors.addElement("yellow");
+	colors.addElement("green");
+	colors.addElement("blue");
+	
+	JList color = new JList(colors);
+	color.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	color.setLayoutOrientation(JList.VERTICAL);
+	color.setVisibleRowCount(-1);
+	JScrollPane colorpane = new JScrollPane(color);
+	colorpane.setPreferredSize(new Dimension(150, 50));
+	
+	btnPanel.add(lAxes);
+	btnPanel.add(txtAxes);
+	btnPanel.add(lColor);
+	btnPanel.add(colorpane);
+	
+	/*
 	btnLeft.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent evt) {
 		    x1 -= 10;
@@ -28,16 +51,7 @@ public class CGMoveALine extends JFrame {
 		    requestFocus();
 		}
 	    });
-	JButton btnRight = new JButton("Move Right");
-	btnPanel.add(btnRight);
-	btnRight.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent evt) {
-		    x1 += 10;
-		    x2 += 10;
-		    canvas.repaint();
-		    requestFocus();
-		}
-	    });
+	*/
 
 	canvas = new DrawCanvas();
 	canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
@@ -58,8 +72,8 @@ public class CGMoveALine extends JFrame {
 	@Override
 	public void paintComponent(Graphics g) {
 	    super.paintComponent(g);
-	    setBackground(Color.BLACK);
-	    g.setColor(Color.WHITE);
+	    setBackground(Color.WHITE);
+	    g.setColor(Color.BLACK);
 	    g.drawLine(x1, y1, x2, y2);
 	}
     }
